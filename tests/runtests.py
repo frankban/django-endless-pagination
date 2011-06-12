e938a92c9ef4f32e3a7738d778ad192b9baa035a
Francesco Banconi <frankban@gmail.com>
2011-06-12 19:36:51 +0200

    Tests are again compatible with older versions of Django.

--- tests/runtests.py
+++ tests/runtests.py
@@ -6,11 +6,15 @@ sys.path.append('..')
 backup = os.environ.get('DJANGO_SETTINGS_MODULE', '')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
-from django.test.simple import DjangoTestSuiteRunner
+try:
+    from django.test.simple import DjangoTestSuiteRunner
+except ImportError:
+    from django.test.simple import run_tests
+else:
+    run_tests = DjangoTestSuiteRunner().run_tests
 
 if __name__ == "__main__":
-    runner = DjangoTestSuiteRunner(verbosity=1)
-    failures = runner.run_tests(['endless_pagination'])
+    failures = run_tests(['endless_pagination'])
     if failures:
         sys.exit(failures)
     os.environ['DJANGO_SETTINGS_MODULE'] = backup
--- tests/settings.py
+++ tests/settings.py
@@ -1,4 +1,5 @@
 DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3'}}
+DATABASE_ENGINE = 'sqlite3'
 ROOT_URLCONF = ''
 SITE_ID = 1
 INSTALLED_APPS = (
