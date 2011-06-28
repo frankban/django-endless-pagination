c7403d774ec8bc6bc736369411ee14d94ce308a0
Francesco Banconi <frankban@gmail.com>
2011-06-28 12:44:55 +0200

    Implemented a way to avoid ajax request if not needed.

--- endless_pagination/static/endless_pagination/js/endless.js
+++ endless_pagination/static/endless_pagination/js/endless.js
@@ -1,6 +1,5 @@
 (function($) {
     $(document).ready(function(){
-        // initializes links for ajax requests
         $("a.endless_more").live("click", function() {
             var container = $(this).closest(".endless_container");
             var loading = container.find(".endless_loading");
@@ -14,9 +13,12 @@
             return false;
         });
         $("a.endless_page_link").live("click", function() {
-            var data = "querystring_key=" + $(this).attr("rel").split(" ")[0];
-            $(this).closest(".endless_page_template").load($(this).attr("href"), data);
-            return false;
+            var page_template = $(this).closest(".endless_page_template");
+            if (!page_template.hasClass("endless_page_skip")) {
+                var data = "querystring_key=" + $(this).attr("rel").split(" ")[0];
+                page_template.load($(this).attr("href"), data);
+                return false;
+            };
         }); 
     });
 })(jQuery);
