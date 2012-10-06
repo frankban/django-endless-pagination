fe2229751ebfd1ed93e749e29c9fca84a1017b6d
Francesco Banconi <frankban@gmail.com>
2012-10-06 15:22:44 +0200

    Base structure for templatetags unittests.

---
+++ endless_pagination/tests/templatetags/test_endless.py
@@ -0,0 +1,37 @@
+"""Endless template tags tests."""
+
+from django.test import TestCase
+from django.test.client import RequestFactory
+
+from endless_pagination.templatetags import endless
+
+
+class PaginateTest(TestCase):
+
+    pass
+
+
+class LazyPaginateTest(TestCase):
+
+    pass
+
+
+class ShowMoreTest(TestCase):
+
+    pass
+
+
+class GetPagesTest(TestCase):
+
+    pass
+
+
+class ShowPagesTest(TestCase):
+
+    pass
+
+
+class ShowCurrentNumberTest(TestCase):
+
+    pass
+
