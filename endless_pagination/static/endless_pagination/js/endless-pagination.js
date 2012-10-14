c7c6e0217d68bf362b7d1238ce4cbec509ed8275
Francesco Banconi <frankban@gmail.com>
2012-10-14 13:40:34 +0200

    Improved new Javascript: add callbacks and new selectors, avoid multiple Ajax call. Reorganized testing project template structure.
    --HG--
    rename : tests/project/templates/multiple_entries_page.html => tests/project/templates/complete/entries_page.html
    rename : tests/project/templates/multiple_items_page.html => tests/project/templates/complete/items_page.html
    rename : tests/project/templates/multiple_objects_page.html => tests/project/templates/complete/objects_page.html
    rename : tests/project/templates/digg_page.html => tests/project/templates/digg/page.html
    rename : tests/project/templates/multiple_entries_page.html => tests/project/templates/multiple/entries_page.html
    rename : tests/project/templates/multiple_items_page.html => tests/project/templates/multiple/items_page.html
    rename : tests/project/templates/multiple_objects_page.html => tests/project/templates/multiple/objects_page.html
    rename : tests/project/templates/twitter_page.html => tests/project/templates/onscroll/page.html
    rename : tests/project/templates/twitter_page.html => tests/project/templates/twitter/page.html

--- endless_pagination/static/endless_pagination/js/endless-pagination.js
+++ endless_pagination/static/endless_pagination/js/endless-pagination.js
@@ -3,46 +3,89 @@
 
     $.fn.endlessPaginate = function(options) {
         var defaults = {
+            // Twitter-style pagination container selector.
+            containerSelector: '.endless_container',
+            // Twitter-style pagination loading selector.
+            loadingSelector: '.endless_loading',
             // Twitter-style pagination link selector.
             moreSelector: 'a.endless_more',
+            // Digg-style pagination page template selector.
+            pageSelector: '.endless_page_template',
             // Digg-style pagination link selector.
             pagesSelector: 'a.endless_page_link',
+            // Callback called when the user clicks to get another page.
+            onClick: function() {},
+            // Callback called when the new page is correctly displayed.
+            onCompleted: function() {},
             // Set this to true to use the paginate-on-scroll feature.
             paginateOnScroll: false,
             // If paginate-on-scroll is on, this margin will be used.
-            paginateOnScrollMargin : 1,
-            // Callback called when the user clicks to get another page.
-            onClick: function() {},
-            // Callback called when the requested data is loaded.
-            onLoaded: function() {},
-            // Callback called when the new page is correctly displayed.
-            onCompleted: function() {}
+            paginateOnScrollMargin : 1
         },
             settings = $.extend(defaults, options);
 
-        var getData = function(link) {
-            return 'querystring_key=' + link.attr('rel').split(' ')[0];
+        var getContext = function(link) {
+            return {
+                querystring_key: link.attr('rel').split(' ')[0],
+                url: link.attr('href')
+            };
         };
 
         return this.each(function() {
             var element = $(this);
+
             // Twitter-style pagination.
             element.find(settings.moreSelector).live('click', function() {
                 var link = $(this),
-                    container = link.closest('.endless_container');
+                    container = link.closest(settings.containerSelector),
+                    loading = container.find(settings.loadingSelector);
+                // Avoid multiple Ajax calls.
+                if (loading.is(':visible')) {
+                    return false;
+                }
                 link.hide();
-                container.find('.endless_loading').show();
-                $.get(link.attr('href'), getData(link), function(data) {
-                    container.before(data);
-                    container.remove();
-                });
+                loading.show();
+                var context = getContext(link);
+                // Fire onClick callback.
+                if (settings.onClick.apply(link.get(0), [context]) !== false) {
+                    var data = 'querystring_key=' + context.querystring_key;
+                    // Send the Ajax request.
+                    $.get(context.url, data, function(fragment) {
+                        container.before(fragment);
+                        container.remove();
+                        // Fire onCompleted callback.
+                        settings.onCompleted.apply(fragment, [context]);
+                    });
+                }
                 return false;
             });
+
+            // On scroll pagination.
+            if (settings.paginateOnScroll) {
+                var win = $(window),
+                    doc = $(document);
+                win.scroll(function(){
+                    if (doc.height() - win.height() -
+                        win.scrollTop() <= settings.paginateOnScrollMargin) {
+                        element.find(settings.moreSelector).click();
+                    }
+                });
+            }
+
             // Digg-style pagination.
             element.find(settings.pagesSelector).live('click', function() {
                 var link = $(this),
-                    page_template = link.closest('.endless_page_template');
-                page_template.load(link.attr('href'), getData(link));
+                    context = getContext(link);
+                // Fire onClick callback.
+                if (settings.onClick.apply(link.get(0), [context]) !== false) {
+                    var page_template = link.closest(settings.pageSelector),
+                        data = 'querystring_key=' + context.querystring_key;
+                    // Send the Ajax request.
+                    page_template.load(context.url, data, function(fragment) {
+                        // Fire onCompleted callback.
+                        settings.onCompleted.apply(fragment, [context]);
+                    });
+                }
                 return false;
             });
         });
