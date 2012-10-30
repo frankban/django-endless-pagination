f81de486a57136fec7d6991f9e1ff562b9ad2336
Francesco Banconi <frankban@gmail.com>
2012-10-30 11:09:04 +0100

    Fix pagination on scroll integration tests.

--- endless_pagination/tests/integration/test_onscroll.py
+++ endless_pagination/tests/integration/test_onscroll.py
@@ -10,7 +10,7 @@ class OnScrollPaginationTest(SeleniumTestCase):
     def test_new_elements_loaded(self):
         # Ensure a new page is loaded on scroll.
         self.get()
-        with self.assertNewElements('object', range(1, 11)):
+        with self.assertNewElements('object', range(1, 21)):
             self.scroll_down()
 
     def test_url_not_changed(self):
@@ -21,27 +21,27 @@ class OnScrollPaginationTest(SeleniumTestCase):
 
     def test_direct_link(self):
         # Ensure direct links work.
-        self.get(page=4)
-        self.assertElements('object', range(16, 21))
-        self.assertIn('page=4', self.selenium.current_url)
+        self.get(page=3)
+        self.assertElements('object', range(21, 31))
+        self.assertIn('page=3', self.selenium.current_url)
 
     def test_subsequent_page(self):
         # Ensure next page is correctly loaded in a subsequent page.
         self.get(page=2)
-        with self.assertNewElements('object', range(6, 16)):
+        with self.assertNewElements('object', range(11, 31)):
             self.scroll_down()
 
     def test_multiple_show_more(self):
         # Ensure new pages are loaded again and again.
         self.get()
         for page in range(2, 5):
-            expected_range = range(1, 5 * page + 1)
+            expected_range = range(1, 10 * page + 1)
             with self.assertSameURL():
                 with self.assertNewElements('object', expected_range):
                     self.scroll_down()
 
     def test_scrolling_last_page(self):
         # Ensure scrolling on the last page is a no-op.
-        self.get(page=10)
-        with self.assertNewElements('object', range(46, 51)):
+        self.get(page=5)
+        with self.assertNewElements('object', range(41, 51)):
             self.scroll_down()
