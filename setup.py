68f67a9cd62574bb9e33f4e4c538cef895548544
Francesco Banconi <frankban@gmail.com>
2012-11-25 18:08:27 +0100

    Python 3 support documented in setup classifiers.

--- setup.py
+++ setup.py
@@ -72,6 +72,7 @@ with VenvLinkDeleted():
             'License :: OSI Approved :: MIT License',
             'Operating System :: OS Independent',
             'Programming Language :: Python',
+            'Programming Language :: Python :: 3',
             'Topic :: Utilities',
         ],
     )
